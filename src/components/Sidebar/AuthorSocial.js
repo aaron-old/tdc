95586cf24bef868d71b572bcd4780493e2e7e4d8
Aaron Young <mustarddevelopment@gmail.com>
2017-05-29 16:42:28 -0700

    Cleared up the author sidebar to a stateless component.

--- src/components/Sidebar/AuthorSocial.js
+++ src/components/Sidebar/AuthorSocial.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
+import './Styles/'
 
 
-export default class extends Component {
 
-    render() {
+const AuthorSocial = () => (
 
-        return (
-            <div id="socialRow" className="text-center col-xs-12">
-                <a href="https://www.facebook.com/thomas.cathey.35?fref=ts" target="_blank">
-                    <i className="fa fa-facebook-square fa-3x"/>
-                </a>
-                <a href="https://twitter.com/ThomasCathey2" target="_blank">
-                    <i className="fa fa-twitter-square fa-3x"/>
-                </a>
-                <a href="#" target="_blank">
-                    <i className="fa fa-google-plus-square fa-3x"/>
-                </a>
-            </div>
-        )
-    }
-}
+    <div id="socialRow" className="text-center col-xs-12">
+        <a href="https://www.facebook.com/thomas.cathey.35?fref=ts" target="_blank">
+            <i className="fa fa-facebook-square fa-3x"/>
+        </a>
+        <a href="https://twitter.com/ThomasCathey2" target="_blank">
+            <i className="fa fa-twitter-square fa-3x"/>
+        </a>
+        <a href="#" target="_blank">
+            <i className="fa fa-google-plus-square fa-3x"/>
+        </a>
+    </div>
+);
+export default AuthorSocial;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,27 +2,20 @@
 import React, { Component } from 'react';
 import SidebarNavigation from './SidebarNavigation';
 import AuthorPhotoRow from './AuthorPhotoRow';
+import AuthorSocialRow from './AuthorSocial';
 
 import './Styles/Sidebar.css';
 
-export default class extends Component {
-
-    constructor(props) {
-
-        super(props);
-
-    }
+const Sidebar = () => (
 
 
-    render() {
-
-        return (
-            <div id="sidebarWrapper">
-                <div className="row`">
-                    <AuthorPhotoRow/>
-                    <SidebarNavigation/>
-                </div>
+        <div id="sidebarWrapper">
+            <div className="row`">
+                <AuthorPhotoRow/>
+                <AuthorSocialRow/>
+                <SidebarNavigation/>
             </div>
-        )
-    }
-}
+        </div>
+);
+
+export default Sidebar;
