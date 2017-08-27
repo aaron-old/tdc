4d7463c08945b0cf27e07fc1364f088b2e0d996a
Aaron Young <mustarddevelopment@gmail.com>
2017-08-27 11:57:06 -0700

    Updating server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
@@ -18,44 +18,34 @@ app.set("port", process.env.PORT || 3001);
 
 const routes = require("./routes/index");
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"));
-    app.use(compression());
-    app.use((err, req, res, next) => {
-        res.status(err.status || 500)
-            .json({
-                status: "error",
-                message: err.message
-            });
-    });
-}else {
-    app.use(logger("dev"));
-    app.use((err, req, res, next) => {
-        res.status(err.status || 500)
-            .json({
-                status: "error",
-                message: err
-            });
-    });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(compression());
+  app.use(logger("dev"));
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+        .json({
+          status: "error",
+          message: err.message
+        });
+  });
+} else {
+  app.use(logger("dev"));
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+        .json({
+          status: "error",
+          message: err
+        });
+  });
 }
-
-
-
-// app.use(session({
-//     secret: process.env.SECRET_KEY,
-//     resave: true,
-//     saveUninitialized: true,
-// }));
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-}
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
@@ -63,16 +53,16 @@ app.get("*", (req, res) => {
 
 app.use("/api", routes);
 
-if(process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"){
-    db.sequelize.sync().then(() => {
-        app.listen(app.get("port"), () => {
-            console.log(`find the server at: http://localhost:${app.get("port")}/`);
-        })
+if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
+  db.sequelize.sync().then(() => {
+    app.listen(app.get("port"), () => {
+      console.log(`find the server at: http://localhost:${app.get("port")}/`);
     })
+  })
 }
 else {
-    app.listen(app.get("port"));
-    db.sequelize.sync();
+  app.listen(app.get("port"));
+  db.sequelize.sync();
 }
 
 module.exports = app;
