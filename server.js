0b7c64713fce18624757a685ad0a1ad1c6dee145
Aaron Young <mustarddevelopment@gmail.com>
2017-08-27 11:46:49 -0700

    Adding the build and updating the package.json file.

--- server.js
+++ server.js
@@ -53,7 +53,10 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+}
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
